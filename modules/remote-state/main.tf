8fdb523d491722675a818ac5a31ee70bf9cbff97
aknysh <andriy.knysh@gmail.com>
2021-01-21 16:50:17 -0500

    Always include component name in workspace name for `remote` backend

--- modules/remote-state/main.tf
+++ modules/remote-state/main.tf
@@ -8,14 +8,6 @@ locals {
   backend_type = module.backend_config.backend_type
   backend      = module.backend_config.backend
 
-  workspace_from_environment_stage = var.include_component_in_workspace_name ? format("%s-%s-%s", module.this.environment, module.this.stage, var.component) : (
-    format("%s-%s", module.this.environment, module.this.stage)
-  )
-
-  workspace = var.workspace != null ? var.workspace : (
-    try(local.backend.workspace, null) != null ? local.backend.workspace : local.workspace_from_environment_stage
-  )
-
   remote_states = {
     s3     = data.terraform_remote_state.s3
     remote = data.terraform_remote_state.remote
--- modules/remote-state/remote.tf
+++ modules/remote-state/remote.tf
@@ -1,3 +1,11 @@
+locals {
+  remote_workspace_from_environment_stage = format("%s-%s-%s", module.this.environment, module.this.stage, var.component)
+
+  remote_workspace = var.workspace != null ? var.workspace : (
+    try(local.backend.workspace, null) != null ? local.backend.workspace : local.remote_workspace_from_environment_stage
+  )
+}
+
 data "terraform_remote_state" "remote" {
   count = local.backend_type == "remote" ? 1 : 0
 
@@ -7,7 +15,7 @@ data "terraform_remote_state" "remote" {
     organization = local.backend.organization
 
     workspaces = {
-      name = local.workspace
+      name = local.remote_workspace
     }
   }
 
