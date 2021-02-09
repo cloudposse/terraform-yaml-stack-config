5e286e9f4054cfb0a9445b3fb5ca951dbcf37f40
aknysh <andriy.knysh@gmail.com>
2021-02-09 12:24:10 -0500

    Use `base_component` to decide whether to include component name in Terraform workspace name for s3 backend

--- modules/remote-state/outputs.tf
+++ modules/remote-state/outputs.tf
@@ -13,6 +13,16 @@ output "base_component" {
   description = "Base component name"
 }
 
+output "s3_workspace_name" {
+  value       = local.s3_workspace
+  description = "Terraform workspace name for the component s3 backend"
+}
+
+output "remote_workspace_name" {
+  value       = local.remote_workspace
+  description = "Terraform workspace name for the component remote backend"
+}
+
 output "outputs" {
   value       = local.outputs
   description = "The outputs of the component"
