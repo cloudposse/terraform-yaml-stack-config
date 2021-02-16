de79a3ab20297f5656b84779c250b82bfe3d0a19
Andriy Knysh <aknysh@users.noreply.github.com>
2021-02-15 21:09:53 -0500

    Add `workspace` to terraform outputs (#13)
    * Add `workspace` to terraform outputs
    
    * Add `TestExamplesStacks` terratest
    
    * Add `TestExamplesStacks` terratest
    
    * Add `TestExamplesStacks` terratest

--- main.tf
+++ main.tf
@@ -3,5 +3,28 @@ data "utils_stack_config_yaml" "config" {
 }
 
 locals {
-  config = [for i in data.utils_stack_config_yaml.config.output : yamldecode(i)]
+  decoded = [for i in data.utils_stack_config_yaml.config.output : yamldecode(i)]
+
+  config = [
+    for stack in local.decoded : {
+      components = {
+        helmfile = stack.components.helmfile,
+        terraform = {
+          for k, v in stack.components.terraform : k => {
+            backend      = v.backend
+            backend_type = v.backend_type
+            env          = v.env
+            settings     = v.settings,
+            vars         = v.vars
+            component    = try(v.component, null)
+            workspace = try(v.backend_type, "") == "remote" ? format("%s-%s", format("%s-%s", try(v.vars.environment, ""), try(v.vars.stage, "")), k) : (
+              try(v.component, null) != null ? format("%s-%s-%s", try(v.vars.environment, ""), try(v.vars.stage, ""), k) : (
+                format("%s-%s", try(v.vars.environment, ""), try(v.vars.stage, ""))
+              )
+            )
+          }
+        }
+      }
+    }
+  ]
 }
---
+++ test/src/examples_stacks_test.go
@@ -0,0 +1,43 @@
+package test
+
+import (
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// Test the Terraform module in examples/complete using Terratest.
+func TestExamplesStacks(t *testing.T) {
+	t.Parallel()
+
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: "../../examples/stacks",
+		Upgrade:      true,
+		// Variables to pass to our Terraform code using -var-file options
+		VarFiles: []string{"fixtures.tfvars"},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the value of an output variable
+	var output interface{}
+	terraform.OutputStruct(t, terraformOptions, "config", &output)
+	config := output.([]interface{})
+
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, len(config), 4)
+	uatConfig := config[3].(map[string]interface{})
+	uatComponents := uatConfig["components"].(map[string]interface{})
+	uatTerraformComponents := uatComponents["terraform"].(map[string]interface{})
+	auroraPostgresComponent := uatTerraformComponents["aurora-postgres"].(map[string]interface{})
+	auroraPostgres2Component := uatTerraformComponents["aurora-postgres-2"].(map[string]interface{})
+	auroraPostgresWorkspace := auroraPostgresComponent["workspace"]
+	auroraPostgres2Workspace := auroraPostgres2Component["workspace"]
+	assert.Equal(t, "uw2-uat", auroraPostgresWorkspace)
+	assert.Equal(t, "uw2-uat-aurora-postgres-2", auroraPostgres2Workspace)
+}
