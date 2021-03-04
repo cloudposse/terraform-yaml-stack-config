ecb7898c09a5886e1456a5a88a6d922389987ced
aknysh <andriy.knysh@gmail.com>
2021-03-03 20:05:13 -0500

    Remove `count` from data sources. Always set `enabled=true` on the `backend` submodule

--- modules/env/main.tf
+++ modules/env/main.tf
@@ -1,15 +1,13 @@
 locals {
-  enabled = module.this.enabled
-  stack   = var.stack != null ? var.stack : format("%s-%s", module.this.environment, module.this.stage)
+  stack = var.stack != null ? var.stack : format("%s-%s", module.this.environment, module.this.stage)
 }
 
 data "utils_stack_config_yaml" "config" {
-  count = local.enabled ? 1 : 0
   input = [format("%s/%s.yaml", var.stack_config_local_path, local.stack)]
 }
 
 locals {
-  config         = try(yamldecode(data.utils_stack_config_yaml.config[0].output[0]), {})
-  env            = try(local.config["components"][var.component_type][var.component]["env"], {})
+  config         = yamldecode(data.utils_stack_config_yaml.config.output[0])
+  env            = local.config["components"][var.component_type][var.component]["env"]
   base_component = try(local.config["components"][var.component_type][var.component]["component"], "")
 }
--- modules/settings/main.tf
+++ modules/settings/main.tf
@@ -1,15 +1,13 @@
 locals {
-  enabled = module.this.enabled
-  stack   = var.stack != null ? var.stack : format("%s-%s", module.this.environment, module.this.stage)
+  stack = var.stack != null ? var.stack : format("%s-%s", module.this.environment, module.this.stage)
 }
 
 data "utils_stack_config_yaml" "config" {
-  count = local.enabled ? 1 : 0
   input = [format("%s/%s.yaml", var.stack_config_local_path, local.stack)]
 }
 
 locals {
-  config         = try(yamldecode(data.utils_stack_config_yaml.config[0].output[0]), {})
-  settings       = try(local.config["components"][var.component_type][var.component]["settings"], {})
+  config         = yamldecode(data.utils_stack_config_yaml.config.output[0])
+  settings       = local.config["components"][var.component_type][var.component]["settings"]
   base_component = try(local.config["components"][var.component_type][var.component]["component"], "")
 }
