75cd7c6d6e17a9c701d4067dbcd1eedcf6039aa4
Matt Calhoun <matt@cloudposse.com>
2021-03-10 14:15:24 -0500

    add aws profile support (#17)
    Co-authored-by: cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>

--- modules/remote-state/s3.tf
+++ modules/remote-state/s3.tf
@@ -19,7 +19,8 @@ data "terraform_remote_state" "s3" {
     key                  = local.backend.key
     dynamodb_table       = local.backend.dynamodb_table
     region               = local.backend.region
-    role_arn             = var.privileged ? null : local.backend.role_arn
+    role_arn             = var.privileged || ! contains(keys(local.backend), "role_arn") ? null : local.backend.role_arn
+    profile              = var.privileged || ! contains(keys(local.backend), "profile") ? null : local.backend.profile
     workspace_key_prefix = var.component
   }
 
