536d418080348eb61ce6b58ddac867a5d57dcc91
aknysh <andriy.knysh@gmail.com>
2021-04-11 00:44:07 -0400

    Use the base component to read the backend config and remote state if a virtual component is provided

--- modules/remote-state/s3.tf
+++ modules/remote-state/s3.tf
@@ -19,9 +19,9 @@ data "terraform_remote_state" "s3" {
     key                  = local.backend.key
     dynamodb_table       = local.backend.dynamodb_table
     region               = local.backend.region
-    role_arn             = var.privileged || ! contains(keys(local.backend), "role_arn") ? null : local.backend.role_arn
-    profile              = var.privileged || ! contains(keys(local.backend), "profile") ? null : local.backend.profile
-    workspace_key_prefix = var.component
+    role_arn             = var.privileged || !contains(keys(local.backend), "role_arn") ? null : local.backend.role_arn
+    profile              = var.privileged || !contains(keys(local.backend), "profile") ? null : local.backend.profile
+    workspace_key_prefix = coalesce(local.base_component, var.component)
   }
 
   defaults = var.defaults
