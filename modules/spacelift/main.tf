ba1277af23ac9f82c04c9eb30d71911b27294c5b
aknysh <andriy.knysh@gmail.com>
2021-05-17 01:15:07 -0400

    Add `spacelift` module and example

--- modules/spacelift/main.tf
+++ modules/spacelift/main.tf
@@ -1,5 +1,5 @@
-data "utils_spacelift_stack_config" "example" {
-  input                      = var.stacks
+data "utils_spacelift_stack_config" "spacelift_stacks" {
+  input                      = [for stack in var.stacks : format(var.stack_config_path_template, stack)]
   process_stack_deps         = var.stack_deps_processing_enabled
   process_component_deps     = var.component_deps_processing_enabled
   process_imports            = var.imports_processing_enabled
@@ -7,5 +7,5 @@ data "utils_spacelift_stack_config" "example" {
 }
 
 locals {
-  spacelift_stacks = yamldecode(data.utils_spacelift_stack_config.example.output)
+  spacelift_stacks = yamldecode(data.utils_spacelift_stack_config.spacelift_stacks.output)
 }
