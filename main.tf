471c88270c791e8fd0053871d5cd71a2931d3677
aknysh <andriy.knysh@gmail.com>
2021-08-21 15:23:41 -0400

    Update to latest `null-label`

--- main.tf
+++ main.tf
@@ -22,9 +22,9 @@ locals {
             stacks       = v.stacks
             deps         = v.deps
             component    = try(v.component, null)
-            workspace = try(v.backend_type, "") == "remote" ? format("%s-%s", format("%s-%s", try(v.vars.environment, module.this.environment), try(v.vars.stage, module.this.stage)), k) : (
-              try(v.component, null) != null ? format("%s-%s-%s", try(v.vars.environment, module.this.environment), try(v.vars.stage, module.this.stage), k) : (
-                format("%s-%s", try(v.vars.environment, module.this.environment), try(v.vars.stage, module.this.stage))
+            workspace = try(v.backend_type, "") == "remote" ? format("%s-%s", format("%s-%s", try(v.vars.environment, ""), try(v.vars.stage, "")), k) : (
+              try(v.component, null) != null ? format("%s-%s-%s", try(v.vars.environment, ""), try(v.vars.stage, ""), k) : (
+                format("%s-%s", try(v.vars.environment, ""), try(v.vars.stage, ""))
               )
             )
           }
