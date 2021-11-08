2e03fd5089828772558d1daf7b60ab4d7e9c5d2f
aknysh <andriy.knysh@gmail.com>
2021-11-08 16:50:45 -0500

    Update `spacelift` module

--- examples/spacelift/main.tf
+++ examples/spacelift/main.tf
@@ -1,9 +1,7 @@
 module "spacelift" {
   source = "../../modules/spacelift"
 
-  stacks                            = var.stacks
   stack_config_path_template        = var.stack_config_path_template
-  stack_config_path                 = "${path.cwd}/stacks"
   stack_deps_processing_enabled     = var.stack_deps_processing_enabled
   component_deps_processing_enabled = var.component_deps_processing_enabled
   imports_processing_enabled        = var.imports_processing_enabled
