ab01064501d35fb3bf5e477d72dd146848acd445
aknysh <andriy.knysh@gmail.com>
2022-02-12 20:00:56 -0500

    Update `utils` provider. Add `ignore_errors` variable to `remote-state`

--- examples/remote-state/main.tf
+++ examples/remote-state/main.tf
@@ -17,3 +17,18 @@ module "remote_state_using_context" {
 
   context = module.this.context
 }
+
+module "remote_state_using_context_ignore_errors" {
+  source = "../../modules/remote-state"
+
+  # Use a wrong component for testing
+  # If `ignore_errors = false`, the `utils` provider will throw the error: Could not find config for the component 'test/test-component-override-wrong' in the stack 'tenant1-ue2-dev'
+  # Note that terraform `try()` does not catch errors from providers, so `try(module.remote_state_using_context_ignore_errors.outputs, {})` will not work
+  ignore_errors = true
+  component     = "test/test-component-override-wrong"
+  tenant        = "tenant1"
+  environment   = "ue2"
+  stage         = "dev"
+
+  context = module.this.context
+}
--- examples/remote-state/outputs.tf
+++ examples/remote-state/outputs.tf
@@ -7,3 +7,8 @@ output "remote_state_using_context" {
   value       = module.remote_state_using_context.outputs
   description = "Component remote state using the provided context"
 }
+
+output "remote_state_using_context_ignore_errors" {
+  value       = module.remote_state_using_context_ignore_errors.outputs
+  description = "Component remote state using wrong component. Errors are ignored in the 'utils' provider"
+}
