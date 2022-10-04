18239bde1692f405a121a5d1b2daf8e2bfd0dace
Andriy Knysh <aknysh@users.noreply.github.com>
2022-10-04 17:13:12 -0400

    Add `atmos_cli_config_path` and `atmos_base_path` variables to `remote-state` module (#53)
    * Add `atmos_cli_config_path` and `atmos_base_path` variables
    
    * Auto Format
    
    Co-authored-by: cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>

---
+++ examples/remote-state/variables.tf
@@ -0,0 +1,11 @@
+variable "atmos_cli_config_path" {
+  type        = string
+  description = "atmos CLI config path"
+  default     = null
+}
+
+variable "atmos_base_path" {
+  type        = string
+  description = "atmos base path to components and stacks"
+  default     = null
+}
