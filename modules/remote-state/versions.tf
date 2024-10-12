c4a06a6037c4a25d96fc311be14317ac31378ec7
Nuru <Nuru@users.noreply.github.com>
2024-10-11 20:07:27 -0700

    Fix Terratest tests

--- modules/remote-state/versions.tf
+++ modules/remote-state/versions.tf
@@ -15,7 +15,7 @@ terraform {
       # We were previously pinning this to <=1.8.0, but changing this each time we had a new version was a pain. As
       # a compromise, we'll pin to a minimum version, but allow any patch version below 2.0.0 and we will make sure
       # that if we make any major/breaking changes in cloudposse/utils, we'll increment to 2.0.0.
-      version = ">= 1.7.1, != 1.4.0, < 2.0.0"
+      version = ">= 1.7.1, < 2.0.0"
     }
   }
 }
